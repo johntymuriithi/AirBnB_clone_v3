0850cbfe5253ce8654dce65317d5c2989d6eb4f8
johntymuriithi <muriithijohn634@gmail.com>
2024-04-02 07:32:44 +0300

    weuh

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,29 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """get obj"""
+
+        if cls and id:
+            if cls in classes.values():
+                all_objects = self.all(cls)
+
+                for value in all_objects.values():
+                    if value.id == id:
+                        return value
+            return
+        return
+    
+    def count(self, cls=None):
+        """count"""
+
+        if not cls:
+            inst_of_all_cls = self.all()
+            return len(inst_of_all_cls)
+        if cls in classes.values():
+            all_inst_of_prov_cls = self.all(cls)
+            return len(all_inst_of_prov_cls)
+        if cls not in classes.values():
+            return
+
